ba060e4a1d828921325e07aed3b620a53edc764f
Edward <edward.an@ucalgary.ca>
2024-09-22 18:48:07 -0600

    added local storage to cart

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,4 +1,4 @@
-import React, {createContext, useState, useReducer} from "react";
+import React, {createContext, useState, useReducer, useEffect} from "react";
 
 export const CartContext = createContext();
 
@@ -7,9 +7,9 @@ const CartContextProvider = (props) => {
 
   const cartItemReducer = (state, action) => {
     switch (action.type) {
-      case "REMOVE_FROM_CART":
+      case "REMOVE_FROM_CART": // Payload parameters: id
         return state.filter(item => item.product._id !== action.id);
-      case "ADD_TO_CART":
+      case "ADD_TO_CART": // Payload parameters: product, quantity
         const exists = state.some(item => item.product._id === action.product._id);
         if (exists) {
           const existingItem = state.find(item => item.product._id === action.product._id);
@@ -19,57 +19,35 @@ const CartContextProvider = (props) => {
           const newItem = { product: action.product, quantity: action.quantity };
           return([...state, newItem]);
         }
-      case "QUANTITY_CHANGE":
+      case "QUANTITY_CHANGE": // Payload parameters: id, newQuantity
         return (state.map(item =>
           item.product._id === action.id
             ? { ...item, quantity: Number(action.newQuantity) }
             : item
         ));
+      case "SET_CART": // Payload parameters: products
+        return ([...action.products]);
       default:
         return state;
     }
   }
 
-  const [itemsInCart, itemsInCartDispatch] = useReducer(cartItemReducer, []);
-    const [isCartDisplayed, setIsCartDisplayed] = useState(false);
-    // const [itemsInCart, setItemsInCart] = useState([]);
+  const [itemsInCart, itemsInCartDispatch] = useReducer(cartItemReducer, [], ()=> {
+    const storedCartItems = localStorage.getItem("cart");
+    return storedCartItems ? JSON.parse(storedCartItems) : [];
+  });
 
-    // const handleQuantityChangeInCart = (id, newQuantity) => {
-    //     setItemsInCart(itemsInCart.map(item =>
-    //       item.product._id === id
-    //         ? { ...item, quantity: Number(newQuantity) }
-    //         : item
-    //     ));
-    //   }
-  
-    // const handleRemoveFromCart = (id) => {
-    //     setItemsInCart(prevItems => prevItems.filter(item => item.product._id !== id));
-    // }
-    
-    // const handleAddToCart = (product, quantity) => {
-    //     const exists = itemsInCart.some(item => item.product._id === product._id);
-    //     if (exists) {
-    //       const existingItem = itemsInCart.find(item => item.product._id === product._id);
-    //       const updatedQuantity = Number(existingItem.quantity) + Number(quantity);
-    
-    //       setItemsInCart(itemsInCart.map(item =>
-    //         item.product._id === product._id
-    //           ? { ...item, quantity: Number(updatedQuantity) }
-    //           : item
-    //       ));
-    //     } else {
-    //       const newItem = { product: product, quantity: quantity };
-    //       setItemsInCart([...itemsInCart, newItem]);
-    //     }
-    //   };
+  const [isCartDisplayed, setIsCartDisplayed] = useState(false);
 
-    return (
-        <CartContext.Provider value={{isCartDisplayed, setIsCartDisplayed, itemsInCart, itemsInCartDispatch}}>
-            {props.children}
-        </CartContext.Provider>
-    );
+  useEffect(()=> {
+    localStorage.setItem("cart", JSON.stringify(itemsInCart));
+  }, [itemsInCart]);
 
-
+  return (
+      <CartContext.Provider value={{isCartDisplayed, setIsCartDisplayed, itemsInCart, itemsInCartDispatch}}>
+          {props.children}
+      </CartContext.Provider>
+  );
 }
 
 export default CartContextProvider;
